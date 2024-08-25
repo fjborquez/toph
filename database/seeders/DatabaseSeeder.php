84f234ca1f204dfe362a3bf619a9627cc22303f2
Francisco Bórquez <fborquez@outlook.com>
2024-08-25 19:04:19 -0400

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(UnitCategoriesSeeder::class);
+        $this->call(UnitOfMeasurementSeeder::class);
+        $this->call(UnitConversionsSeeder::class);
     }
 }
