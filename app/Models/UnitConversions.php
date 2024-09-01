702ed3f05077e64688149ec276af8aa28df43ce6
Francisco Bórquez <fborquez@outlook.com>
2024-09-01 12:45:43 -0400

    get unit of measurement by id

---
+++ app/Models/UnitConversions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UnitConversions extends Model
+{
+    use HasFactory;
+
+    protected $table = 'unit_conversions';
+
+    protected $fillable = [
+        'from_unit_id',
+        'to_unit_id',
+        'factor'
+    ];
+
+    public function fromUnit(): BelongsTo
+    {
+        return $this->belongsTo(UnitCategory::class, 'from_unit_id');
+    }
+
+    public function toUnit(): BelongsTo
+    {
+        return $this->belongsTo(UnitCategory::class, 'to_unit_id');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\UnitOfMeasurementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/unit-of-measurement', [UnitOfMeasurementController::class, 'list']);
+Route::get('/unit-of-measurement/{id}', [UnitOfMeasurementController::class, 'get']);
