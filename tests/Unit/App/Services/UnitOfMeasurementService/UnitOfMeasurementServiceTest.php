e269f9de0c66538e9058d341eeb32bd83733b1f4
Francisco Bórquez <fborquez@outlook.com>
2024-12-11 12:45:34 -0300

    fix unit test

--- tests/Unit/App/Services/UnitOfMeasurementService/UnitOfMeasurementServiceTest.php
+++ tests/Unit/App/Services/UnitOfMeasurementService/UnitOfMeasurementServiceTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Enums\UOMEnum;
 use App\Exceptions\ResourceNotFoundException;
 use App\Services\UnitOfMeasurementService\UnitOfMeasurementService;
 use Illuminate\Database\Eloquent\Collection;
@@ -13,11 +12,36 @@ class UnitOfMeasurementServiceTest extends TestCase
 
     private $model;
 
-    protected function setUp(): void
+    public function setUp(): void
     {
         parent::setUp();
         $this->mockedUnitOfMeasurementService = new UnitOfMeasurementService;
         $this->model = Mockery::mock('overload:App\Models\UnitOfMeasurement');
+    }
+
+    public function test_should_retrieve_filtered_unit_of_measurement_list()
+    {
+        $mockQueryBuilder = Mockery::mock('overload:'.QueryBuilder::class);
+
+        $mockQueryBuilder->shouldReceive('for')
+            ->andReturnSelf();
+        $mockQueryBuilder->shouldReceive('allowedFilters')
+            ->andReturnSelf();
+        $mockQueryBuilder->shouldReceive('get')
+            ->once()
+            ->andReturn(new Collection([
+                [
+                    'id' => 13,
+                    'description' => 'Unit',
+                    'abbreviation' => 'ud',
+                    'category_id' => 4,
+                    'created_at' => null,
+                    'updated_at' => null,
+                ],
+            ]));
+
+        $result = $this->mockedUnitOfMeasurementService->getList();
+        $this->assertEquals(1, $result->count());
     }
 
     public function test_get_should_return_unit_of_measurement_when_id_does_exists()
